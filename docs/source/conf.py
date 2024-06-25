20a70d29b504512423457e5fb18c7e454c8dd4f3
ggenelot <77398544+ggenelot@users.noreply.github.com>
2024-06-25 14:28:52 +0200

    Fixed problems with notebooks
    Changed lines 132 in python312> lib > site-packages > choco > compat.py
    
    if win32 or jython:
        time_func = time.clock
    
    to
    if win32 or jython:
        time_func = time.perf_counter

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
+    'nbshinx',
 ]
 
 intersphinx_mapping = {
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -16,6 +16,7 @@ Contents
 .. toctree::
 
    context
+   notebooks/test
    impacts
    IAM
    functions
