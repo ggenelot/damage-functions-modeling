eaa312292f405b7affda4debfdefd14039a37d90
ggenelot <77398544+ggenelot@users.noreply.github.com>
2024-06-25 14:19:16 +0200

    Revert "Added some code to store the runs in the database"
    This reverts commit 9e762c70b2ad40fec8a4753f1c3b334532e9cb33.

--- run.py
+++ run.py
@@ -4,23 +4,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import warnings
 import pandas as pd
-import http.client
-import json
-from datetime import datetime
-
-# Prepare the API to store the results in the database
-
-api_token = api_token
-
-conn = http.client.HTTPSConnection("app.nocodb.com")
-
-headers = {
-    'xc-token': api_token,
-    'Content-Type': 'application/json'
-}
-
-
-
 
 # Load model
 model = pysd.read_vensim('WILIAM_v1.3/WILIAM.mdl',
@@ -36,7 +19,6 @@ variables_FUND_names = variables_FUND['Py Name'].values
 
 initial_time = 2005
 final_time = 2040
-time_step = 0.25
 
 # Suppress specific warnings to avoid cluttering the output
 warnings.filterwarnings('ignore', category=RuntimeWarning)
@@ -50,36 +32,3 @@ run = model.run(progress=True,
                 )
 
 warnings.resetwarnings()
-
-
-
-
-# Store the run in the database
-
-# JSON payload for the new record
-new_record_data = {
-    "Date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),  # Example timestamp format
-    "End_year": initial_time,
-    "Name_of_the_run": f'run {initial_time} to {final_time}',
-    "Start_year": final_time,
-    "Stored_variables": "variable1, variable2",
-    "Timestep": time_step,
-    "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-    "created_by": "user_id",
-    "updated_at": None,  # Replace with actual value if needed
-    "updated_by": None  # Replace with actual value if needed
-}
-
-# Convert new_record_data to JSON format
-json_data = json.dumps(new_record_data)
-
-# API endpoint for adding records
-endpoint = "/api/v2/tables/myi4g8i6dsl36by/records"
-
-# Send POST request
-conn.request("POST", endpoint, json_data, headers)
-
-res = conn.getresponse()
-data = res.read()
-
-print(data.decode("utf-8"))
