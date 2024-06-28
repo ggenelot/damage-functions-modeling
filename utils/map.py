02015ceefc8c4a26211ff0f9be4fe1cb57522743
ggenelot <77398544+ggenelot@users.noreply.github.com>
2024-06-28 16:21:05 +0200

    Fix minor bugs

--- utils/map.py
+++ utils/map.py
@@ -1,8 +1,9 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import xarray as xr
 
 
-def map_variable(data, year, europe=True, cmap="cividis", shapefile_location='../../../WILIAM_v1.3/geography/output_countries/countries.shp'):
+def map_variable(data, year, europe=True, cmap="cividis", shapefile_location='../../../WILIAM_v1.3/geography/output_countries/countries.shp', boundary_linewidth=0.2):
 
     # Load variable name from the df
     variable = data.columns[2]
@@ -26,13 +27,15 @@ def map_variable(data, year, europe=True, cmap="cividis", shapefile_location='..
 
     # Plot the map
     fig, ax = plt.subplots(figsize=(10, 10))
-    map.plot(ax=ax, color='white', edgecolor='lightgray')
+    
     map_phenomenon.plot(ax=ax, 
                         cmap=cmap,
                         column=variable, 
                         legend=True, 
                         legend_kwds={'label': "Agricultural damage in FUND (% of agricultural production)", 
                                     'orientation': "horizontal"})
+    
+    map.boundary.plot(ax=ax, linewidth=boundary_linewidth, edgecolor='lightgray')
 
     # Set the title and axis labels
     ax.set_title(f'European Union Map of {variable} in {year}')
@@ -49,19 +52,19 @@ def map_variable(data, year, europe=True, cmap="cividis", shapefile_location='..
     # Show the plot
     plt.show()
 
-    def load_variable(variable, dataset_name='results_run_2060.nc'):
-        """
-        Load variable data from a NetCDF dataset.
+def load_variable(variable, dataset_name='results_run_2060.nc'):
+    """
+    Load variable data from a NetCDF dataset.
 
-        Parameters:
-        variable (str): The name of the variable to load.
+    Parameters:
+    variable (str): The name of the variable to load.
 
-        Returns:
-        pandas.DataFrame: The loaded variable data as a DataFrame.
-        """
+    Returns:
+    pandas.DataFrame: The loaded variable data as a DataFrame.
+    """
 
-        ds = xr.open_dataset(f'../../../results/{dataset_name}')
-        data = ds[variable].to_dataframe().reset_index()
-        ds.close()
+    ds = xr.open_dataset(f'../../../results/{dataset_name}')
+    data = ds[variable].to_dataframe().reset_index()
+    ds.close()
 
-        return data
+    return data
